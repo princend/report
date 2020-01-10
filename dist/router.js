308229852dd472f5087baf944e93c6c875322e36
Princend <waxdji3g4y3c04@gmail.com>
2020-01-11 01:40:19 +0800

    ignore

--- dist/router.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const user_route_1 = require("./routes/user.route");
-exports.router = [
-    new user_route_1.default()
-];
